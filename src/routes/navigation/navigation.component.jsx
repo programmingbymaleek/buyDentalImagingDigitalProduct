8e0d723b92c1aba5764d72cf47c8bb59d0241144
wisdom maliki <wisdommaliki19@gmail.com>
2022-04-03 18:33:48 -0600

    updated navigationpages

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,13 +4,13 @@ import './navigation.styles.scss';
 const Navbar = () => {
   return (<Fragment>
     <div className="navigation">
-      <Link className="logo" to='/'> <h2>WilMatech Enterprise</h2></Link>
+      <Link className="logo" to='/buyDentalImagingDigitalProduct'> <h2>WilMatech Enterprise</h2></Link>
       <div className="nav-links-container">
-        <Link className="nav-link" to='/sign-in'>Sign-in</Link>
-        <Link className="nav-link" to='/sign-up'>
+        <Link className="nav-link" to='sign-in'>Sign-in</Link>
+        <Link className="nav-link" to='sign-up'>
           Create an account
         </Link>
-        <Link className="nav-link" to='/about'>
+        <Link className="nav-link" to='about'>
           About
         </Link>
       </div>
