a9d4fe9c99d7983b26e72f7776db38f967a54dbc
wisdom maliki <wisdommaliki19@gmail.com>
2022-06-03 15:46:00 -0600

    updated the nav component

--- src/components/cart_icon_component/cart_icon.jsx
+++ src/components/cart_icon_component/cart_icon.jsx
@@ -2,13 +2,16 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping_icon.svg";
 import './cart.icon.styles.scss';
 import { useContext } from 'react'
 import { CartUserContext } from "../../context.apis/cart_context/cart_context";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 const CartIcon = () => {
   const { toggler, cartCount } = useContext(CartUserContext)
 
   return (<div className="cart-icon-container">
-    <ShoppingIcon className="shopping-icon" onClick={toggler} />
+    <FontAwesomeIcon className="shopping-icon" icon={faCartShopping} onClick={toggler} style={{ color: 'white' }} />
+    {/* <ShoppingIcon className="shopping-icon" onClick={toggler} /> */}
     <span className="item-count">{cartCount}</span>
   </div>)
 
